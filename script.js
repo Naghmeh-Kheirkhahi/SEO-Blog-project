523e75b4d2b7a67859753a68fb9bbaa64c7f0dbc
naghmeh <melodycharitable4774@gmail.com>
2024-08-18 22:46:41 +0200

    completed the form and replaced the gif

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 
 let subBtn = document.getElementById('subBtn');
+
 subBtn.addEventListener('click', ()=> {
 
-    // let form = document.getElementById('');
+    let form = document.querySelector('form');
+    form.style.display= 'none';
 
+    document.querySelector('.registerForm').innerHTML +=
+    `<p class="answerForm">Thank you for contacting us. We will get back to you as soon as possible.</p>`
 })
