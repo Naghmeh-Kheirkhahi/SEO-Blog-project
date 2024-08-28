7b163432744826e849836225da309dc9ab2f786f
naghmeh <melodycharitable4774@gmail.com>
2024-08-29 00:04:14 +0200

    did the search bar need to complete the posts on the blog

--- blog.js
+++ blog.js
@@ -7,3 +7,21 @@ window.onscroll = function() {
         header.classList.remove("scrolled");
     }
 };
+
+
+
+
+let searchBtn = document.getElementById('searchBtn');
+let searchBar = document.getElementById('searchBar');
+
+
+searchBtn.addEventListener('click', ()=>{
+    console.log('The search has done!');
+})
+
+
+searchBar.addEventListener("keypress", function (e){
+      if (e.key === "Enter"){
+        console.log('The search has done!');
+      }
+  })
